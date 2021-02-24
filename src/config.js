734113cfb48b66141ee2d13b50dced22c90a1a7d
Gary Windham <windhamg@email.arizona.edu>
2021-02-24 15:49:42 -0700

    re-add esri API endpoint

--- src/config.js
+++ src/config.js
@@ -1,19 +1,19 @@
 const prod = {
     endpoints: [
         {
-          name: "enumValues",
-          endpoint: "https://frruryqr87.execute-api.us-west-2.amazonaws.com/dev",
-          region: "us-west-2"
+            name: "enumValues",
+            endpoint: "https://frruryqr87.execute-api.us-west-2.amazonaws.com/dev",
+            region: "us-west-2"
         },
         {
-          name: "regPublish",
-          endpoint: "https://frruryqr87.execute-api.us-west-2.amazonaws.com/dev",
-          region: "us-west-2"
+            name: "regPublish",
+            endpoint: "https://frruryqr87.execute-api.us-west-2.amazonaws.com/dev",
+            region: "us-west-2"
         },
         {
-          name: "zipCheck",
-          endpoint: "https://frruryqr87.execute-api.us-west-2.amazonaws.com/dev",
-          region: "us-west-2"
+            name: "zipCheck",
+            endpoint: "https://frruryqr87.execute-api.us-west-2.amazonaws.com/dev",
+            region: "us-west-2"
         },
     ]
 };
@@ -21,39 +21,49 @@ const prod = {
 const nonprod = {
     endpoints: [
         {
-          name: "enumValues",
-          endpoint: "https://isgr6ntyzj.execute-api.us-west-2.amazonaws.com/nonprod",
-          region: "us-west-2"
+            name: "enumValues",
+            endpoint: "https://isgr6ntyzj.execute-api.us-west-2.amazonaws.com/nonprod",
+            region: "us-west-2"
         },
         {
-          name: "regPublish",
-          endpoint: "https://isgr6ntyzj.execute-api.us-west-2.amazonaws.com/nonprod",
-          region: "us-west-2"
+            name: "regPublish",
+            endpoint: "https://isgr6ntyzj.execute-api.us-west-2.amazonaws.com/nonprod",
+            region: "us-west-2"
         },
         {
-          name: "zipCheck",
-          endpoint: "https://isgr6ntyzj.execute-api.us-west-2.amazonaws.com/nonprod",
-          region: "us-west-2"
+            name: "zipCheck",
+            endpoint: "https://isgr6ntyzj.execute-api.us-west-2.amazonaws.com/nonprod",
+            region: "us-west-2"
+        },
+        {
+            name: "esri",
+            endpoint: "https://frruryqr87.execute-api.us-west-2.amazonaws.com/dev",
+            region: "us-west-2"
         },
     ]
 };
- 
+
 const dev = {
     endpoints: [
         {
-          name: "enumValues",
-          endpoint: "https://isgr6ntyzj.execute-api.us-west-2.amazonaws.com/nonprod",
-          region: "us-west-2"
+            name: "enumValues",
+            endpoint: "https://isgr6ntyzj.execute-api.us-west-2.amazonaws.com/nonprod",
+            region: "us-west-2"
         },
         {
-          name: "regPublish",
-          endpoint: "https://isgr6ntyzj.execute-api.us-west-2.amazonaws.com/nonprod",
-          region: "us-west-2"
+            name: "regPublish",
+            endpoint: "https://isgr6ntyzj.execute-api.us-west-2.amazonaws.com/nonprod",
+            region: "us-west-2"
         },
         {
-          name: "zipCheck",
-          endpoint: "https://isgr6ntyzj.execute-api.us-west-2.amazonaws.com/nonprod",
-          region: "us-west-2"
+            name: "zipCheck",
+            endpoint: "https://isgr6ntyzj.execute-api.us-west-2.amazonaws.com/nonprod",
+            region: "us-west-2"
+        },
+        {
+            name: "esri",
+            endpoint: "https://isgr6ntyzj.execute-api.us-west-2.amazonaws.com/dev",
+            region: "us-west-2"
         },
     ]
 };
@@ -63,9 +73,9 @@ const configMap = {
     'prod': prod,
     'nonprod': nonprod,
 };
-  
+
 const config = configMap[process.env.REACT_APP_STAGE];
-  
+
 export const recaptcha_site_key = '6LeAd1QaAAAAAEkFTsbdFCGMbm0SjNgqMe6LIn6R';
 export const age_threshold = 75;
 export const endpoints = config.endpoints;
